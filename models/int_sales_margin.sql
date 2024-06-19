cbe154ad4644957ced336b754aff17e19d890e36
Suzie Kelly <suziek72@gmail.com>
2024-06-19 12:57:00 +0000

    init sales margin creating purchase cost and margin columns

---
+++ models/int_sales_margin.sql
@@ -0,0 +1,13 @@
+select 
+    products_id
+    ,date_date
+    ,orders_id
+    ,revenue
+    ,quantity
+    ,CAST(purchse_price AS FLOAT64)
+    ,ROUND(s.quantity*CAST(p.purchse_price AS FLOAT64),2) AS purchase_cost
+    ,s.revenue - ROUND(s.quantity*CAST(p.purchse_price AS FLOAT64),2) AS margin
+
+from {{ref("stg_raw__sales")}} s 
+left join {{ref("stg_raw__product")}} p 
+using (products_id)
